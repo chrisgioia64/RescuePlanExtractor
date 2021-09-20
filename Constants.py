62c22bbfb8809b41ccdf95fea6e04da3dd8669dc
Christopher Gioia <christophergioia64@gmail.com>
2021-09-19 21:34:48 -0700

    update constants.py

--- Constants.py
+++ Constants.py
@@ -7,6 +7,7 @@ LOGS_DIR = "logs/"
 STATES_DIR = "states/"
 TEMPLATES_DIR = "template/"
 LOCALS_DIR = "local/"
+LOCALS_GDRIVE_LINK = "https://drive.google.com/drive/folders/1ixRAsqCEpsau9l2-NzSkUSzVWmZOUr7s"
 
 # Filename of report template (pdf and txt extensions added)
 REPORT_TEMPLATE = "ReportTemplate"
